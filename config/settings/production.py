d956a7480a156fa1dc9088c6febb676c60d79951
tdarde <thomas.darde@scilicium.com>
2021-10-19 14:51:49 +0200

    add sdjango

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["django"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["scilicium.com"])
+ALLOWED_HOSTS += ['django']
 DEBUG = True
 # DATABASES
 # ------------------------------------------------------------------------------
