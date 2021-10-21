cca1620b532a90d090c1a5ada3b8e763e50c32d2
tdarde <thomas.darde@scilicium.com>
2021-10-21 17:19:53 +0200

    fix prod

--- config/settings/production.py
+++ config/settings/production.py
@@ -16,7 +16,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["scilicium.com"])
 ALLOWED_HOSTS += ['django']
-CSRF_TRUSTED_ORIGINS = ["test-192-168-101-113.vm.openstack.genouest.org","django-192-168-101-113.vm.openstack.genouest.org"]
+CSRF_TRUSTED_ORIGINS = ["test-192-168-101-113.vm.openstack.genouest.org"]
 DEBUG = True
 # DATABASES
 # ------------------------------------------------------------------------------
