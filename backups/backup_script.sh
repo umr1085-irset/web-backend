d927fa757080e1272aa3a5db7c0f3c3b99bc2535
root <root@web-irset.genouest.org>
2022-09-16 14:51:42 +0200

    backup script

--- backups/backup_script.sh
+++ backups/backup_script.sh
@@ -8,17 +8,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CELERY_BROKER_URL="${REDIS_URL}"
 DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 LOCAL_BACKUP_DIR="$DIR""/archives/"
-REMOTE_BACKUP_DIR="/groups/irset/archives/web/Irset_admin/archives/"
+REMOTE_BACKUP_DIR="/groups/irset/archives/web/genoViewer/"
 DATE=`eval date +%Y%m%d`
 
 echo "Writing Postgres dump"
 /usr/local/bin/docker-compose -f "$DIR""/../production.yml" exec -T postgres sh -c "pg_dump --clean -U $POSTGRES_USER $POSTGRES_DB > /backups/DB_backup"
-mv "$DIR""/DB_backup" "$DIR""/../scilicium_django_react/media/"
-echo "Building archive"
-tar -pzcf  "$LOCAL_BACKUP_DIR""archive-$DATE"".tar.gz" "$DIR""/../scilicium_django_react/media/"
-rm "$DIR""/../scilicium_django_react/media/DB_backup"
-
-cd $LOCAL_BACKUP_DIR
-ls -1t  | tail -n +3 | xargs rm -f
-echo "Rsync.."
-rsync -av --delete --exclude=".*" $LOCAL_BACKUP_DIR $REMOTE_BACKUP_DIR
+mv "$DIR""/DB_backup" "$REMOTE_BACKUP_DIR"
