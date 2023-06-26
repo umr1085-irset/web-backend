87e282efd9c981a5222b01022868b553f0c3d41d
PaulRivaud <paulrivaud.info@gmail.com>
2023-06-26 16:07:16 +0200

    delete test

--- scilicium_django_react/datasets/models.py
+++ scilicium_django_react/datasets/models.py
@@ -136,3 +136,11 @@ class Dataset(models.Model):
         super(Dataset, self).save(*args, **kwargs)
         self.datasetId = "d" + str(self.id)
         super(Dataset, self).save()
+
+@receiver(models.signals.pre_delete, sender=Dataset)
+def auto_delete_loomfile_on_delete(sender, instance, **kwargs):
+    # Delete the folder
+    local_path = f"{instance.datasetId}"
+    unix_path = settings.MEDIA_ROOT + "/datasets/loom/admin/" + local_path
+    if(os.path.exists(unix_path)):
+        shutil.rmtree(unix_path)
