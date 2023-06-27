f977d6466f1d89b0596422ee567f9bd8e705e997
PaulRivaud <paulrivaud.info@gmail.com>
2023-06-27 14:32:09 +0200

    loom file rm test

--- scilicium_django_react/datasets/models.py
+++ scilicium_django_react/datasets/models.py
@@ -145,7 +145,7 @@ class Dataset(models.Model):
 def auto_delete_loomfile_on_delete(sender, instance, **kwargs):
     # Delete the folder
     local_path = f"{instance.loom.loomId.replace('l','')}"
-    #unix_path = settings.MEDIA_ROOT + "/datasets/loom/admin/" + local_path
-    #if(os.path.exists(unix_path)):
-    #    shutil.rmtree(unix_path)
-    #Loom.objects.filter(loomId=instance.loom.loomId).delete()
+    unix_path = settings.MEDIA_ROOT + "/datasets/loom/admin/" + local_path
+    if(os.path.exists(unix_path)):
+       shutil.rmtree(unix_path)
+    Loom.objects.filter(loomId=instance.loom.loomId).delete()
